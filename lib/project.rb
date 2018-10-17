74b21412d401368a97d44ca317807f5903405847
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-17 20:10:18 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,13 @@
-class Project
+class project
   attr_accessor :title, :backers
-
-  def initialize(title)
-    @title = title
+  
+  def initalize(title)
+    @title = title 
     @backers = []
-  end
-
+  end 
+  
   def add_backer(backer)
-    @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    @backers<< backer 
+    backer.project(self) unless backer.backed_projects.include?(self)
   end
 end
